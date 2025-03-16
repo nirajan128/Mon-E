4a069dafbb64e4315ab279dee22c5c0d4e408fd8
Nirajan <nirajanshrestha672@gmail.com>
2025-03-16 12:58:08 -0400

    [Section 0: project initiated]: Server setup

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+//Middleware
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+
+app.get("/",(req,res) =>{
+    res.json("HEllo");
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
