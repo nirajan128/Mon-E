4e4cbf1768c28bf542798b103801d863eaf92793
Nirajan <nirajanshrestha672@gmail.com>
2025-03-16 21:05:38 -0400

    [Section 1.3]: Frontend setup - Fonts, colors, and layout : CLIENT

---
+++ client/src/Component/pages/Homepage.tsx
@@ -0,0 +1,19 @@
+import "../../App.css"
+function HomePage() {
+    return(
+        <div id="homeContainer" className="container-fluid">
+           <div className="row">
+            <div className="col-sm-12 col-lg-6 d-flex justify-content-center align-items-center" id="logoContainer">
+                <div id="logo">
+                    <h1>MonE</h1>
+                </div>
+            </div>
+            <div className="col-sm-12 col-lg-6" id="formContainer">
+
+            </div>
+           </div>
+        </div>
+    )
+}
+
+export default HomePage;
